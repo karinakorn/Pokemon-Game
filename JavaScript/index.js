baafd6aa0c682760b0614f470f0fa481b8899276
MatiasG <matias.a.garcia@hotmail.com>
2019-12-23 19:42:51 -0300

    23/12

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,8 +2,46 @@ var mentalismo = new Ataque("mentalismo", "psiquico", 3, 5);
 var psyd = new Ataque("psyd", "psiquico", 3, 5);
 
 
+
+
+
 var dormir = new Ataque("psyd", "psiquico", 3, 5);
 var duck = new Ataque("duck", "psiquico", 3, 5);
+
+
+
+
+
+const pokemon = {
+    0: {
+        img: "",
+        ataques: {
+
+        },
+        propriedades: {
+            tipo: "",
+            debilidad: ""
+        }
+    },
+    1: {
+        img: "",
+        ataques: {
+            0: mentalismo,
+            1: dormir
+        },
+        propriedades: {
+            tipo: "",
+            debilidad: ""
+        }
+    }
+}
+
+
+var player = 1;
+var enemy = 0;
+new Pokemon(pokemon[i].img)
+
+
 
 var jugador1 = new Jugador("Player 1", [abra]);
 var enemgio = new Jugador("Gary", [psyduck]);
@@ -11,9 +49,36 @@ var enemgio = new Jugador("Gary", [psyduck]);
 var abra = new Pokemon(01, "https://img.pokemondb.net/sprites/black-white/normal/abra.png", [mentalismo, dormir], "psiquico",
     "fantasma", 10);
 
-var psyduck = new Pokemon(02, "https://img.pokemondb.net/sprites/black-white/normal/psyduck.png", [psyd, duck], "psiquico", "fantasma", 10)
+var psyduck = new Pokemon(02, "https://img.pokemondb.net/sprites/black-white/normal/psyduck.png", [psyd, duck], "psiquico", "fantasma", 30);
+
+var lapras = new Pokemon(03, "https://img.pokemondb.net/sprites/black-white/normal/lapras.png", [rayo, helada], "hielo", "fuego", 10);
+
+var gastly = new Pokemon(04, "https://img.pokemondb.net/sprites/black-white/normal/gastly.png", [], "fantasma", "tierra", 10);
+
+var mew = new Pokemon(05, "https://img.pokemondb.net/sprites/black-white/normal/mew.png", [], "leyenda", "normal", 10);
+
+var geodude = new Pokemon(06, "https://img.pokemondb.net/sprites/black-white/normal/geodude.png", [], "roca", "agua", 10);
+
+var zubat = new Pokemon(07, "https://img.pokemondb.net/sprites/black-white/normal/zubat-f.png", [], "volador", "electrico", 10);
+
+var jigglypuff = new Pokemon(08, "https://img.pokemondb.net/sprites/black-white/normal/jigglypuff.png", [], "normal", "leyenda", 10);
+
+var squirtle = new Pokemon(09, "https://img.pokemondb.net/sprites/black-white/normal/squirtle.png", [], "agua", "planta", 10);
+
+var charmander = new Pokemon(10, "https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/charmander.png", [], "fuego", "agua", 10);
+
+var mankey = new Pokemon(11, "https://img.pokemondb.net/sprites/black-white/normal/mankey.png", [], "peleador", "fuego", 10);
+
+var pidgey = new Pokemon(12, "https://img.pokemondb.net/sprites/black-white/normal/pidgey.png", [], "volador", "electrico", 10);
+
+var machop = new Pokemon(13, "https://img.pokemondb.net/sprites/black-white/normal/machop.png", [], "roca", "agua", 10);
+
+
+
+
+
+
+
+
 
 // JQUERY
-$(document).ready {
-    $(img).hide()
-}
