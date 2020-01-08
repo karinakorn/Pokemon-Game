b0e1ea8563045e02dd4b6dd31b1e2a286de09c12
Karina Korn <karinakorn90@gmail.com>
2020-01-08 19:44:14 -0300

    Up to date

--- JavaScript/Controlador.js
+++ JavaScript/Controlador.js
@@ -3,3 +3,35 @@ class Controlador {
 
     }
 }
+
+//REVERRR !!!!!!!!!!!!!
+
+function dropMenu(element) {
+    document.getElementById(element).classList.toggle("active");
+}
+
+document.getElementById("elegir-pokemon").addEventListener("mouseover", function(){
+    dropMenu("menu-pokemon");
+});
+document.getElementById("elegir-ataque").addEventListener("mouseover", function(){
+    dropMenu("menu-ataque");
+});
+
+var menupoke, menuataq;
+document.getElementById("elegir-pokemon").addEventListener("mouseout", function(){
+    menupoke = setTimeout(function(){
+        dropMenu("menu-pokemon");
+    }, 1000)
+});
+document.getElementById("elegir-ataque").addEventListener("mouseout", function(){
+    menuataq = setTimeout(function(){
+        dropMenu("menu-ataque");
+    }, 1000)
+});
+
+document.getElementById("menu-pokemon").addEventListener("mouseenter", function(){
+    clearTimeout(menupoke);
+});
+document.getElementById("menu-ataque").addEventListener("mouseenter", function(){
+    clearTimeout(menuataq);
+});
