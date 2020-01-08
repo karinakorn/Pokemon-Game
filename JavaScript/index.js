0de595c9b11c4e49179d03ffc0676f76a69eee7c
MatiasG <matias.a.garcia@hotmail.com>
2020-01-08 19:38:59 -0300

    08/01-Lista-Pokemon

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,84 +1,26 @@
-var mentalismo = new Ataque("mentalismo", "psiquico", 3, 5);
-var psyd = new Ataque("psyd", "psiquico", 3, 5);
+var mentalismo = new Ataque("mentalismo", "fantasma", 300, 5);
+var psiquico = new Ataque("psiquico", "psiquico", 300, 5);
+var lanzallamas = new Ataque("lanzallamas", "fuego", 300, 5);
+var llamarada = new Ataque("llamarada", "fuego", 300, 5);
+var golpePuño = new Ataque("golpePuño", "roca", 300, 5);
+var patada = new Ataque("patada", "normal", 300, 5);
+var rayoHielo = new Ataque("rayoHielo", "hielo", 300, 5);
+var placaje = new Ataque("placaje", "normal", 300, 5);
+var tornado = new Ataque("tornado", "volador", 300, 5);
+var ataqueAla = new Ataque("ataqueAla", "volador", 300, 5);
+var lanzaAgua = new Ataque("lanzaAgua", "agua", 300, 5);
+var burbujas = new Ataque("burbujas", "agua", 300, 5);
+var cantar = new Ataque("cantar", "normal", 300, 5);
+var eco = new Ataque("eco", "volador", 300, 5);
+var pesadilla = new Ataque("pesadilla", "fantasma", 300, 5);
+var asustar = new Ataque("asustar", "fantasma", 300, 5);
+var rayo = new Ataque("rayo", "normal", 300, 5);
+var especial = new Ataque("especial", "normal", 2000, 5);
 
 
 
 
 
-var dormir = new Ataque("psyd", "psiquico", 3, 5);
-var duck = new Ataque("duck", "psiquico", 3, 5);
 
-
-
-
-
-const pokemon = {
-    0: {
-        img: "",
-        ataques: {
-
-        },
-        propriedades: {
-            tipo: "",
-            debilidad: ""
-        }
-    },
-    1: {
-        img: "",
-        ataques: {
-            0: mentalismo,
-            1: dormir
-        },
-        propriedades: {
-            tipo: "",
-            debilidad: ""
-        }
-    }
-}
-
-
-var player = 1;
-var enemy = 0;
-new Pokemon(pokemon[i].img)
-
-
-
+var player = new Pokemon(pokemon[0].img["back"], pokemon[0].ataques[0], pokemon[0].propiedades)
 var jugador1 = new Jugador("Player 1", [abra]);
-var enemgio = new Jugador("Gary", [psyduck]);
-
-var abra = new Pokemon(01, "https://img.pokemondb.net/sprites/black-white/normal/abra.png", [mentalismo, dormir], "psiquico",
-    "fantasma", 10);
-
-var psyduck = new Pokemon(02, "https://img.pokemondb.net/sprites/black-white/normal/psyduck.png", [psyd, duck], "psiquico", "fantasma", 30);
-
-var lapras = new Pokemon(03, "https://img.pokemondb.net/sprites/black-white/normal/lapras.png", [rayo, helada], "hielo", "fuego", 10);
-
-var gastly = new Pokemon(04, "https://img.pokemondb.net/sprites/black-white/normal/gastly.png", [], "fantasma", "tierra", 10);
-
-var mew = new Pokemon(05, "https://img.pokemondb.net/sprites/black-white/normal/mew.png", [], "leyenda", "normal", 10);
-
-var geodude = new Pokemon(06, "https://img.pokemondb.net/sprites/black-white/normal/geodude.png", [], "roca", "agua", 10);
-
-var zubat = new Pokemon(07, "https://img.pokemondb.net/sprites/black-white/normal/zubat-f.png", [], "volador", "electrico", 10);
-
-var jigglypuff = new Pokemon(08, "https://img.pokemondb.net/sprites/black-white/normal/jigglypuff.png", [], "normal", "leyenda", 10);
-
-var squirtle = new Pokemon(09, "https://img.pokemondb.net/sprites/black-white/normal/squirtle.png", [], "agua", "planta", 10);
-
-var charmander = new Pokemon(10, "https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/charmander.png", [], "fuego", "agua", 10);
-
-var mankey = new Pokemon(11, "https://img.pokemondb.net/sprites/black-white/normal/mankey.png", [], "peleador", "fuego", 10);
-
-var pidgey = new Pokemon(12, "https://img.pokemondb.net/sprites/black-white/normal/pidgey.png", [], "volador", "electrico", 10);
-
-var machop = new Pokemon(13, "https://img.pokemondb.net/sprites/black-white/normal/machop.png", [], "roca", "agua", 10);
-
-
-
-
-
-
-
-
-
-// JQUERY
--- JavaScript/pokemon.js
+++ JavaScript/pokemon.js
@@ -6,5 +6,218 @@ class Pokemon {
         this.tipo = tipo;
         this.debilidad = debilidad;
         this.vidas = vidas;
+
     }
+}
+const listaPokemon = {
+    0: {
+        name: "abra",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/abra.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/abra.png"
+        },
+        ataques: {
+            0: "mentalismo",
+            1: "psiquico"
+
+        },
+        propriedades: {
+            tipo: "psiquico",
+            debilidad: "fantasma",
+            vidas: 1000
+        },
+    },
+    1: {
+        name: "Charmander",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/charmander.png",
+            "back": "https://img.pokemondb.net/sprites/heartgold-soulsilver/back-normal/charmander.png"
+        },
+        ataques: {
+            0: "lanzallamas",
+            1: "llamarada"
+        },
+        propriedades: {
+            tipo: "fuego",
+            debilidad: "agua",
+            vidas: 1000
+        }
+    },
+    2: {
+        name: "Machop",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/machop.png",
+            "back": "https://img.pokemondb.net/sprites/heartgold-soulsilver/back-normal/machop.png"
+        },
+        ataques: {
+            0: "golpePuño",
+            1: "patada"
+        },
+        propriedades: {
+            tipo: "roca",
+            debilidad: "agua",
+            vidas: 1000
+        }
+    },
+    3: {
+        name: "Lapras",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/lapras.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/lapras.png"
+        },
+        ataques: {
+            0: "rayoHielo",
+            1: "placaje"
+        },
+        propriedades: {
+            tipo: "hielo",
+            debilidad: "fuego",
+            vidas: 1000
+        }
+    },
+    4: {
+        name: "Pidgey",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/pidgey.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/pidgey.png"
+        },
+        ataques: {
+            0: "tornado",
+            1: "ataqueAla"
+        },
+        propriedades: {
+            tipo: "volador",
+            debilidad: "hielo",
+            vidas: 1000
+        }
+    },
+    5: {
+        name: "Mankey",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/mankey.png",
+            "back": "https://img.pokemondb.net/sprites/heartgold-soulsilver/back-normal/mankey.png"
+        },
+        ataques: {
+            0: "golpePuño",
+            1: "patada"
+        },
+        propriedades: {
+            tipo: "normal",
+            debilidad: "volador",
+            vidas: 1000
+        }
+    },
+    6: {
+        name: "Squirtle",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/squirtle.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/squirtle.png"
+        },
+        ataques: {
+            0: "lanzaAgua",
+            1: "burbujas"
+        },
+        propriedades: {
+            tipo: "agua",
+            debilidad: "normal",
+            vidas: 1000
+        }
+    },
+    7: {
+        name: "Jigglypuff",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/jigglypuff.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/jigglypuff.png"
+        },
+        ataques: {
+            0: "cantar",
+            1: "placaje"
+        },
+        propriedades: {
+            tipo: "normal",
+            debilidad: "volador",
+            vidas: 1000
+        },
+    },
+    8: {
+        name: "Zubat",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/zubat-f.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/zubat-f.png"
+        },
+        ataques: {
+            0: "eco",
+            1: "ataqueAla"
+        },
+        propriedades: {
+            tipo: "volador",
+            debilidad: "hielo",
+            vidas: 1000
+        }
+    },
+    9: {
+        name: "Geodude",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/geodude.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/geodude.png"
+        },
+        ataques: {
+            0: "golpePuño",
+            1: "placaje"
+        },
+        propriedades: {
+            tipo: "roca",
+            debilidad: "agua",
+            vidas: 1000
+        }
+    },
+    10: {
+        name: "Gastly",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/gastly.png",
+            "back": "https://img.pokemondb.net/sprites/heartgold-soulsilver/back-normal/gastly.png"
+        },
+        ataques: {
+            0: "pesadilla",
+            1: "asustar"
+        },
+        propriedades: {
+            tipo: "fantasma",
+            debilidad: "normal",
+            vidas: 1000
+        }
+    },
+    11: {
+        name: "Mew",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/mew.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/mew.png"
+        },
+        ataques: {
+            0: "rayo",
+            1: "mentalismo"
+        },
+        propriedades: {
+            tipo: "normal",
+            debilidad: "volador",
+            vidas: 1000
+        }
+    },
+    12: {
+        name: "Psyduck",
+        img: {
+            "front": "https://img.pokemondb.net/sprites/black-white/normal/psyduck.png",
+            "back": "https://img.pokemondb.net/sprites/black-white/back-normal/psyduck.png"
+        },
+        ataques: {
+            0: "especial",
+            1: "placaje",
+        },
+        propriedades: {
+            tipo: "psiquico",
+            debilidad: "null",
+            vidas: 2000
+        }
+    }
+
 }
